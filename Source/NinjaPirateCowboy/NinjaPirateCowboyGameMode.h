4cb8c58f6ff3a06d3351c7daa43473d06c6a29a7
Kieron McNamee <kieron93@gmail.com>
2016-10-20 00:14:54 +0100

    Editor Changes

---
+++ Source/NinjaPirateCowboy/NinjaPirateCowboyGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "NinjaPirateCowboyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NINJAPIRATECOWBOY_API ANinjaPirateCowboyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
